2814f7f3fccefabb62f424d4ed41deb16cf337b2
AgustinGonzalez <xenrak@gmail.com>
2017-02-25 20:24:27 -0300

    Correciones de UX
    Centrado términos y condiciones, agrregada una tílde
    
    Correcciones menores de animaciones en forma de registro

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.controller('layoutController', function($scope, $http, env){
 
         var AnimacionSalida = function(obj){
             RemoverAnimaciones(obj);
-            $(obj).find('i').replaceWith('<i name="icon" class="fa fa-spinner fa-pulse" aria-hidden="true"></i>').toggle(true);
+            $(obj).find('i:first').replaceWith('<i name="icon" class="fa fa-spinner fa-pulse" aria-hidden="true"></i>').toggle(true);
             $(obj).find('input, h3').addClass('animated fadeOutLeft');
             $(obj).find('button').prop('disabled', true);
             $(obj).find('span').toggle(false);
@@ -116,15 +116,16 @@ app.controller('layoutController', function($scope, $http, env){
 
         var AnimacionError = function(mensaje, obj){
             RemoverAnimaciones(obj);
-            $(obj).find('i').replaceWith('<i class="fa fa-times" aria-hidden="true"></i>');
+            $(obj).find('i:first').replaceWith('<i class="fa fa-times" aria-hidden="true"></i>');
             $(obj).find('button span').html(mensaje).toggle(true);
             $(obj).find('input, h3').addClass('animated fadeInRight');
-            $(obj).find('button').prop('disabled', false).addClass('animated bounceIn');
+            $(obj).find('button').addClass('animated bounceIn');
 
             setTimeout(function(){
-                $(form).find('span').html("Aprender&nbsp")
-                $(form).find('i').replaceWith('<i></i>')
-            }, 4000 );
+                $(obj).find('button').prop('disabled', false).addClass('animated bounceIn');
+                $(form).find('span').html("Aprender&nbsp");
+                $(form).find('i:first').replaceWith('<i></i>');
+            }, 2000 );
         }
 
         var form = $('.reg-form');
